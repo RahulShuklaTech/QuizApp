8d7c894a623ccdaca2f775d083cc8028cfb95701
Rahul <rahuls@live.ca>
2021-05-10 01:16:46 +0530

    added progress bar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import {  useState } from "react";
+import { useEffect, useState } from "react";
 import Question from "./Question";
 import Options from './Options';
 import questions from "../data"
@@ -7,40 +7,77 @@ import ShowConfetti from "./ShowConfetti"
 const answers = [];
 export default function Main() {
     let [currenQuestion, setCurrentQuestion] = useState(0);
-    let [score,setScore] = useState(0)
-    let [showConfetti,setShowConfetti] = useState(false)
-    let [disable,setDisable] = useState(false)
-    
-    const handleClick = (index,e) => {
+    let [score, setScore] = useState(0)
+    let [showConfetti, setShowConfetti] = useState(false)
+    let [disable, setDisable] = useState(false)
+    let [width, setWidth] = useState(100)
+    let [gotAnswer, setGotAnswer] = useState(false)
+    useEffect(() => {
+        setWidth(100);
+        let barWidth;
+
+        barWidth = setInterval(() => {
+
+            if (currenQuestion < 5) {
+                setWidth((bar) => {
+                    if (bar - 0.5 < 0 || gotAnswer) {
+                        if (!gotAnswer && currenQuestion < 5) {
+                            setCurrentQuestion(cur => cur + 1);
+                            if (currenQuestion === answers.length) {
+                                answers.push("No Answer");
+                            }
+                        }
+                        clearInterval(barWidth)
+                        setGotAnswer(!gotAnswer)
+                    }
+                    return bar - .1;
+                });
+            }
+        })
+
+
+       
+        return (() => { if (barWidth) clearInterval(barWidth) })
+    }, [currenQuestion, gotAnswer]);
+
+
+    if (answers.length === questions.length) return <Results result={answers} ques={questions} />
+
+
+    function handleClick(index, e = "") {
+
         answers.push(index)
+       
+        setGotAnswer(true)
         setDisable(true)
         let correctAnswer = questions[currenQuestion].correctOption;
-        if(index === correctAnswer){
-            setScore(current => current + 1 );
-            e.style.backgroundColor = "green";
+        if (index === correctAnswer) {
+            setScore(current => current + 1);
+            if (e) e.style.backgroundColor = "green";
             setShowConfetti(true)
-            
-        }else{
-            e.style.backgroundColor = "red";
+
+        } else {
+            if (e) e.style.backgroundColor = "red";
         }
         setTimeout(() => {
-            e.style.backgroundColor = "black";
-            currenQuestion < (questions.length-1) ? setCurrentQuestion(current => current + 1 ) : setCurrentQuestion(0);
+            if (e) e.style.backgroundColor = "black";
+            setCurrentQuestion(current => current + 1)
             setShowConfetti(false)
             setDisable(false)
-        },3000)
+        }, 3000)
 
     }
-    
 
-    return answers.length === questions.length  && !disable ? <Results result = {answers} ques = {questions}/>:
-       ( 
+
+
+    return (
         <div className="main">
-            {showConfetti &&  <ShowConfetti />}
-            <div className ="score"><h3>Score:{score}</h3></div>
-            <div className ="question"><Question question={questions[currenQuestion].question} /></div>
-            <Options options={questions[currenQuestion].options} handleClick={handleClick} disable = {disable} />
-            
+            {showConfetti && <ShowConfetti />}
+            <div className="progress-bar" style={{ width: width + '%' }}></div>
+            <div className="score"><h3>Score:{score}</h3></div>
+            <div className="question"><Question question={questions[currenQuestion].question} /></div>
+            <Options options={questions[currenQuestion].options} handleClick={handleClick} disable={disable} />
+
         </div>
     )
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./results.css"
 
 const Results = ({result,ques}) => {
-    console.log(ques)
+    console.log(result)
+
     return (
         <div className ="results">
             {result.map( (item,index) => 
-                        <div className = "row"><div className = "result-question">{ques[index].question}</div>
-                        <div className = {checkAnswer(item,ques[index].correctOption)}>{ques[index].options[item]}</div> 
+                        <div className = "row" key ={index}><div className = "result-question">{ques[index].question}</div>
+                        <div className = {checkAnswer(item,ques[index].correctOption)}>{item === "No Answer" ? "No Answer": ques[index].options[item]}</div> 
                         </div>                    
             )} 
        </div>
